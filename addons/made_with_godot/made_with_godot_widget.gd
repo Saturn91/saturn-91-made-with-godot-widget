aa940697cac3a49ed98cd757cb58a6821aeb73fc
Manuel Geissberger <manuel.geissberger@gmail.com>
2025-11-14 11:47:35 +0100

    animate dots

--- addons/made_with_godot/made_with_godot_widget.gd
+++ addons/made_with_godot/made_with_godot_widget.gd
@@ -8,19 +8,37 @@ class_name MadeWidthGodotWidget extends CenterContainer
 
 var original_dev_text := ""
 
+# Animation state for loading dots
+var _loading_anim_time := 0.0
+var _loading_dot_state := 0
+const _LOADING_DOTS = [".", "..", "..."]
+var _loading_base_text := ""
+
 func _ready() -> void:
 	original_dev_text = developer_label.text
+	_loading_base_text = loadingLabel.text.strip_edges()
 	update_ui()
 
 func _process(delta: float) -> void:
-	if MadeWithGodotSource.is_fetching and not loadingBg.visible:
-		loadingBg.show()
-		loadingLabel.show()
-		
-	if !MadeWithGodotSource.is_fetching and loadingBg.visible:
-		loadingBg.hide()
-		loadingLabel.hide()
-	
+	if MadeWithGodotSource.is_fetching:
+		if not loadingBg.visible:
+			loadingBg.show()
+			loadingLabel.show()
+		# Animate loading dots
+		_loading_anim_time += delta
+		if _loading_anim_time >= 0.5:
+			_loading_anim_time = 0.0
+			_loading_dot_state = (_loading_dot_state + 1) % _LOADING_DOTS.size()
+			loadingLabel.text = _loading_base_text + _LOADING_DOTS[_loading_dot_state]
+	else:
+		if loadingBg.visible:
+			loadingBg.hide()
+			loadingLabel.hide()
+		# Reset animation state
+		_loading_anim_time = 0.0
+		_loading_dot_state = 0
+		loadingLabel.text = _loading_base_text + _LOADING_DOTS[0]
+
 	# Check if new data is available
 	if MadeWithGodotSource.has_new_data:
 		update_ui()
